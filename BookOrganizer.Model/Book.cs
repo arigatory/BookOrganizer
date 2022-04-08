909d0156e766f94c7ab17e4e24d66a8c5f11f00e
Ivan MSI <arigatory@gmail.com>
2022-04-08 20:08:16 +0300

    Add project files.

---
+++ BookOrganizer.Model/Book.cs
@@ -0,0 +1,10 @@
+﻿namespace BookOrganizer.Model
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int TotalPages { get; set; }
+
+    }
+}
